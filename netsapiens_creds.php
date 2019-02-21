a97d4caa34e24ffbd92c24887442fce35983b68b
SpectrumVoIP <sswafford@spectrumvoip.com>
2019-02-21 15:13:34 -0600

    Create netsapiens_creds.php

---
+++ netsapiens_creds.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php -q
+<?PHP
+
+$path = "/usr/local/SAN/api_backups/";
+
+$client_id     = 'my_client_id';
+$client_secret = 'my_secret';
+$username      = 'my_username';
+$password      = "my_password";
+
+$base_url = "https://core3-dal.mydomain.com/";
+
+?>
